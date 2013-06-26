4dd98bd3f989e16aa75c1848c99312622173862e
Kyle Raftogianis <kylerafto@gmail.com>
2013-06-25 21:35:42 -0700

    Move dateTime to internal module

--- Network/Email/Parse.hs
+++ Network/Email/Parse.hs
@@ -10,8 +10,6 @@ module Network.Email.Parse
 
 import           Control.Applicative
 import           Data.Attoparsec              (Parser)
-import qualified Data.Attoparsec              as A
-import qualified Data.Attoparsec.Char8        as A8
 import           Data.Attoparsec.Combinator
 import           Data.Char
 import           Data.List
@@ -20,8 +18,6 @@ import qualified Data.Text                    as T
 import           Data.Text.Encoding
 import qualified Data.Text.Lazy               as L
 import           Data.Text.Lazy.Builder
-import           Data.Time
-import           Data.Time.Calendar.WeekDate
 
 import           Network.Email.Parse.Internal
 
@@ -56,63 +52,3 @@ unstructured = fromElements <$> many element
   where
     element = T.concat     <$> many1 encodedWord
           <|> decodeLatin1 <$> textToken
-
--- | Parse a date and time. Currently, non-numeric timezones (such as \"PDT\")
--- are considered equivalent to UTC time.
-dateTime :: Parser ZonedTime
-dateTime = do
-    wday  <- optional dayOfWeek
-    zoned <- zonedTime
-
-    let (_, _, expected) =
-            toWeekDate . localDay . zonedTimeToLocalTime $ zoned
-    case wday of
-        Just actual | actual /= expected -> fail
-            "day of week does not match date"
-        _                                -> return ()
-
-    return zoned
-  where
-    comma     = 44
-    colon     = 58
-
-    dayOfWeek = lexeme dayName <* character comma
-    localTime = LocalTime <$> date <*> timeOfDay
-    zonedTime = ZonedTime <$> localTime <*> timeZone
-
-    date      = do
-        d <- lexeme A8.decimal
-        m <- lexeme month
-        y <- lexeme year
-        failNothing "invalid date" $ fromGregorianValid y m d
-
-    year      =              digits 4
-            <|> (+ 1900) <$> digits 3
-            <|> adjust   <$> digits 2
-      where
-        adjust n | n < 50    = 2000 + n
-                 | otherwise = 1900 + n
-
-    timeOfDay = do
-        h <- lexeme (digits 2)
-        m <- character colon *> lexeme (digits 2)
-        s <- option (0 :: Int) (character colon *> lexeme (digits 2))
-        failNothing "invalid time of day" $
-            makeTimeOfDayValid h m (fromIntegral s)
-
-    timeZone  = minutesToTimeZone <$> timeZoneOffset <|> return utc
-      where
-        timeZoneOffset = A8.signed $ do
-            hh <- digits 2
-            mm <- digits 2
-            if mm >= 60
-                then fail "invalid time zone"
-                else return $ hh * 60 + mm
-
-    listIndex = choice . map (\(n, s) -> n <$ A.string s) . zip [1..]
-    dayName   = listIndex [ "Mon", "Tue", "Wed", "Thu"
-                          , "Fri", "Sat", "Sun"
-                          ]
-    month     = listIndex [ "Jan", "Feb", "Mar", "Apr", "May", "Jun"
-                          , "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-                          ]
--- Network/Email/Parse/Internal.hs
+++ Network/Email/Parse/Internal.hs
@@ -19,6 +19,8 @@ module Network.Email.Parse.Internal
     , textToken
     , quotedString
     , encodedWord
+      -- * Date and time specification
+    , dateTime
       -- * Address specification
     , addrSpec
       -- * Combinators
@@ -29,6 +31,7 @@ import           Control.Applicative
 import           Data.Attoparsec            (Parser)
 import qualified Data.Attoparsec            as A
 import qualified Data.Attoparsec.Char8      as A8
+import           Data.Attoparsec.Combinator
 import qualified Data.Attoparsec.Zepto      as Z
 import           Data.Bits
 import qualified Data.ByteString            as B
@@ -40,6 +43,8 @@ import qualified Data.ByteString.Unsafe     as B
 import           Data.List
 import           Data.Monoid
 import qualified Data.Text                  as T
+import           Data.Time
+import           Data.Time.Calendar.WeekDate
 import           Data.Word
 
 import qualified Network.Email.Charset      as CS
@@ -228,6 +233,66 @@ hexPair = do
     hex w | w >= 48 && w <= 57 = w - 48
           | w >= 65 && w <= 90 = w - 55
           | otherwise          = 255
+
+-- | Parse a date and time. Currently, non-numeric timezones (such as \"PDT\")
+-- are considered equivalent to UTC time.
+dateTime :: Parser ZonedTime
+dateTime = do
+    wday  <- optional dayOfWeek
+    zoned <- zonedTime
+
+    let (_, _, expected) =
+            toWeekDate . localDay . zonedTimeToLocalTime $ zoned
+    case wday of
+        Just actual | actual /= expected -> fail
+            "day of week does not match date"
+        _                                -> return ()
+
+    return zoned
+  where
+    comma     = 44
+    colon     = 58
+
+    dayOfWeek = lexeme dayName <* character comma
+    localTime = LocalTime <$> date <*> timeOfDay
+    zonedTime = ZonedTime <$> localTime <*> timeZone
+
+    date      = do
+        d <- lexeme A8.decimal
+        m <- lexeme month
+        y <- lexeme year
+        failNothing "invalid date" $ fromGregorianValid y m d
+
+    year      =              digits 4
+            <|> (+ 1900) <$> digits 3
+            <|> adjust   <$> digits 2
+      where
+        adjust n | n < 50    = 2000 + n
+                 | otherwise = 1900 + n
+
+    timeOfDay = do
+        h <- lexeme (digits 2)
+        m <- character colon *> lexeme (digits 2)
+        s <- option (0 :: Int) (character colon *> lexeme (digits 2))
+        failNothing "invalid time of day" $
+            makeTimeOfDayValid h m (fromIntegral s)
+
+    timeZone  = minutesToTimeZone <$> timeZoneOffset <|> return utc
+      where
+        timeZoneOffset = A8.signed $ do
+            hh <- digits 2
+            mm <- digits 2
+            if mm >= 60
+                then fail "invalid time zone"
+                else return $ hh * 60 + mm
+
+    listIndex = choice . map (\(n, s) -> n <$ A.string s) . zip [1..]
+    dayName   = listIndex [ "Mon", "Tue", "Wed", "Thu"
+                          , "Fri", "Sat", "Sun"
+                          ]
+    month     = listIndex [ "Jan", "Feb", "Mar", "Apr", "May", "Jun"
+                          , "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+                          ]
 
 -- | Parse an email address, stripping out whitespace and comments.
 addrSpec :: Parser B.ByteString
