b2c9ba2962619a4e401cf398a9b9bf9c6624da0c
Kyle Raftogianis <kylerafto@gmail.com>
2013-06-26 01:18:03 -0700

    Improve parsing documentation

--- Network/Email/Parse.hs
+++ Network/Email/Parse.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 -- | Parsing of common header values.
+--
+-- The parsers exported here are lexeme parsers; that is, they consume
+-- comments or whitespace that follows, but expect no leading comments or
+-- whitespace. This makes combining parsers easier and more efficient. The
+--  only place whitespace is explicitly skipped is at the beginning of a
+-- string, and this is done automatically by 'parseHeader'.
 module Network.Email.Parse
     ( module Network.Email.Parse.Address
     , module Network.Email.Parse.DateTime
