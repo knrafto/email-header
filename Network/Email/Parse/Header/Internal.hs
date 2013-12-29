37ee6d58b53e0b8ce55f46d4d5307bea1c3e90f5
Kyle Raftogianis <kylerafto@gmail.com>
2013-12-28 19:09:32 -0800

    Simplify internal parsing

--- Network/Email/Parse/Header/Internal.hs
+++ Network/Email/Parse/Header/Internal.hs
@@ -2,8 +2,8 @@
 -- | Parsing of basic header syntax, and helper functions.
 module Network.Email.Parse.Header.Internal
     ( -- * Monad failure
-      failNothing
-    , failLeft
+      parseMaybe
+    , parseEither
       -- * Whitespace
     , fws
     , cfws
@@ -29,6 +29,7 @@ import           Control.Monad
 import           Data.Attoparsec              (Parser)
 import qualified Data.Attoparsec              as A
 import qualified Data.Attoparsec.Char8        as A8
+import           Data.Attoparsec.Combinator
 import qualified Data.Attoparsec.Zepto        as Z
 import           Data.Bits
 import qualified Data.ByteString              as B
@@ -43,15 +44,17 @@ import           Data.Monoid
 import qualified Data.Text                    as T
 import           Data.Word
 
-import qualified Network.Email.Charset as CS
+import qualified Network.Email.Charset              as CS
+import           Network.Email.Parse.Header.Builder ((<+>))
+import qualified Network.Email.Parse.Header.Builder as B
 
 -- | Return a 'Just' value, and 'fail' a 'Nothing' value.
-failNothing :: Monad m => String -> Maybe a -> m a
-failNothing s = maybe (fail s) return
+parseMaybe :: Monad m => String -> Maybe a -> m a
+parseMaybe s = maybe (fail s) return
 
 -- | Return a 'Right' value, and 'fail' a 'Left' value.
-failLeft :: Monad m => Either String a -> m a
-failLeft = either fail return
+parseEither :: Monad m => Either String a -> m a
+parseEither = either fail return
 
 -- | Run a 'Builder' as a strict 'B.ByteString'.
 toByteString :: Builder -> B.ByteString
@@ -63,18 +66,18 @@ fws = A8.skipSpace
 
 -- | Parse a comment, including all nested comments.
 comment :: Parser B.ByteString
-comment = A8.char '(' *> A.scan (1 :: Int, False) f <* A8.char ')'
+comment = A8.char '(' *> A.scan (0 :: Int, False) f <* A8.char ')'
   where
     f (!n, True ) _ = Just (n, False)
     f (!n, False) w = case w2c w of
         '('  -> Just (n + 1, False)
-        ')'  -> if n == 1 then Nothing else Just (n - 1, False)
+        ')'  -> if n == 0 then Nothing else Just (n - 1, False)
         '\\' -> Just (n, True)
         _    -> Just (n, False)
 
 -- | Skip any comments or folding whitespace.
 cfws :: Parser ()
-cfws = fws <* optional (comment *> cfws)
+cfws = () <$ fws `sepBy` comment
 
 -- | Parse a value, surrounded by whitespace.
 padded :: Parser a -> Parser a
@@ -93,6 +96,20 @@ digits n = do
     unless (B.all A8.isDigit_w8 s) $
         fail $ "expected " ++ show n ++ " digits"
     return $ B.foldl' (\a w -> 10*a + fromDigit w) 0 s
+
+-- | Parse a hexadecimal pair.
+hexPair :: Parser Word8
+hexPair = decode <$> hexDigit <*> hexDigit
+  where
+    decode a b      = shiftL a 4 .|. b
+    hexDigit        = fromHexDigit <$> A.satisfy isHexDigit
+    isHexDigit w    = w >= 48 && w <= 57
+                   || w >= 64 && w <= 70
+                   || w >= 97 && w <= 102
+    fromHexDigit w
+        | w >= 97   = w - 87
+        | w >= 64   = w - 54
+        | otherwise = w - 48
 
 -- | Parse an token lexeme consisting of all printable characters, but
 --  disallowing the specified special characters.
@@ -119,51 +136,13 @@ token = tokenWith "()<>@,;:\\\"/[]?="
 textToken :: Parser B.ByteString
 textToken = A.takeWhile1 (not . A8.isSpace_w8)
 
--- | Parse an escaped 'B.ByteString'.
-unescape
-    :: (Char -> Bool)    -- ^ Detect a possible escape sequence.
-    -> Z.Parser Builder  -- ^ Escape sequence parser.
-    -> B.ByteString      -- ^ Input 'B.ByteString'.
-    -> Either String B.ByteString
-unescape p decode str
-    | B.any p' str = Z.parse (toByteString <$> go mempty) str
-    | otherwise    = return str
-  where
-    p' = p . w2c
-
-    go acc = do
-        s    <- Z.takeWhile (not . p')
-        done <- Z.atEnd
-        if done
-            then return (acc <> byteString s)
-            else do
-                b <- decode
-                go (acc <> byteString s <> b)
-
--- | Scan for the end of a string.
-scanString :: Char -> Parser B.ByteString
-scanString end = A.scan False f
-  where
-    f True _ = Just False
-    f _    w = case w2c w of
-        c | c == end -> Nothing
-        '\\'         -> Just True
-        _            -> Just False
-
--- | Parse a raw (undecoded) quoted-string.
-rawQuotedString :: Parser B.ByteString
-rawQuotedString = A8.char '"' *> scanString '"' <* A8.char '"'
-
 -- | Parse a quoted-string.
 quotedString :: Parser B.ByteString
-quotedString = do
-    s <- rawQuotedString
-    failLeft $ unescape (\c -> c == '\\' || c == '\r') decode s
+quotedString = toByteString <$ A8.char '"' <*> B.many go <* A8.char '"'
   where
-    decode = decodeEscape <$> Z.take 2
-    decodeEscape s
-        | s == "\r\n" = mempty
-        | otherwise   = word8 (B.unsafeIndex s 1)
+    go = mempty <$ A.string "\r\n" 
+     <|> A8.char '\\' *> B.anyWord8
+     <|> B.satisfy (/= '"')
 
 -- | Parse an encoded word, as per RFC 2047.
 encodedWord :: Parser T.Text
@@ -171,59 +150,47 @@ encodedWord = do
     _       <- A.string "=?"
     charset <- B8.unpack <$> tokenWith "()<>@,;:\"/[]?.="
     _       <- A8.char '?'
-    method  <- decodeQuoted  <$ A.satisfy (`B.elem` "Qq") <|>
-               Base64.decode <$ A.satisfy (`B.elem` "Bb")
+    method  <- quoted       <$ A.satisfy (`B.elem` "Qq") <|>
+               base64String <$ A.satisfy (`B.elem` "Bb")
     _       <- A8.char '?'
-    enc     <- A8.takeWhile (/= '?')
+    enc     <- CS.decode charset <$> method
     _       <- A.string "?="
-    CS.decode charset <$> failLeft (method enc)
+    return enc
   where
-    decodeQuoted = unescape (\c -> c == '=' || c == '_') decode
-      where
-        decode    = unquote . B.unsafeHead =<< Z.take 1
-        unquote w = case w2c w of
-            '_' -> return $ char8 ' '
-            '=' -> word8 <$> hexPair
-            _   -> error "unquote: impossible case"
+    base64String = do s <- A8.takeWhile (/= '?')
+                      parseEither (Base64.decode s)
+    quoted       = toByteString <$> B.many go
 
--- | Parse a 'Word8' represented as a pair of hex digits.
-hexPair :: Z.Parser Word8
-hexPair = do
-    s <- Z.take 2
-    let !a = hex (B.unsafeHead s)
-        !b = hex (B.unsafeIndex s 1)
-    if a .|. b /= 255
-        then return $! (a `shiftL` 4) .|. b
-        else fail "invalid hex escape"
+    go           = char8 ' ' <$ A8.char '_'
+               <|> word8 <$ A8.char '=' <*> hexPair
+               <|> B.satisfy (\c -> not (A8.isSpace c || c == '?'))
+
+-- | Return a quoted string as-is.
+scanString :: Char -> Parser Builder
+scanString end = byteString <$> A8.scan False f
   where
-    hex w | w >= 48 && w <= 57 = w - 48
-          | w >= 65 && w <= 90 = w - 55
-          | otherwise          = 255
+    f True  _       = Just False
+    f False c
+        | c == end  = Nothing
+        | c == '\\' = Just True
+        | otherwise = Just False
 
 -- | Parse an email address, stripping out whitespace and comments.
 addrSpec :: Parser B.ByteString
-addrSpec = toByteString . mconcat <$>
-    sequence [localPart, char8 '@' <$ padded (A8.char '@'), domain]
+addrSpec = toByteString <$> (localPart <+> padded (B.char8 '@') <+> domain)
   where
-    wrap c b  = char8 c <> b <> char8 c
-    dotSep p  = mconcat . intersperse (char8 '.') <$>
-                A.sepBy1 p (padded $ A8.char '.')
+    dotSep p      = p `B.sepBy1` padded (B.char8 '.')
 
-    addrAtom  = byteString <$> atom
-    addrQuote = wrap '"' . byteString <$> rawQuotedString
+    addrAtom      = byteString <$> atom
+    addrQuote     = B.char8 '"' <+> scanString '"' <+> B.char8 '"'
+    domainLiteral = B.char8 '[' <+> scanString ']' <+> B.char8 ']'
 
-    localPart = dotSep (addrAtom <|> addrQuote)
-    domain    = dotSep addrAtom <|> domainLiteral
+    localPart     = dotSep (addrAtom <|> addrQuote)
+    domain        = dotSep addrAtom <|> domainLiteral
 
 -- | Parse an address specification in angle brackets.
 angleAddrSpec :: Parser B.ByteString
 angleAddrSpec = A8.char '<' *> padded addrSpec <* A8.char '>'
-
--- | Parse a domain literal.
-domainLiteral :: Parser Builder
-domainLiteral = wrap <$ A8.char '[' <*> scanString ']' <* A8.char8 ']'
-  where
-    wrap s = char8 '[' <> byteString s <> char8 ']'
 
 -- | @optionalSepBy p sep@ applies /zero/ or more occurrences of @p@,
 -- separated by @sep@. Null entries between separators will be filtered
