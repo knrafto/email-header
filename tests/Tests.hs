1bd74cd038be7e3fd2b22b4b15ca89b26ea188b0
Kyle Raftogianis <kylerafto@gmail.com>
2014-03-07 16:07:27 -0800

    Test all non-MIME headers

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -4,6 +4,7 @@ module Main
     ) where
 
 import           Control.Applicative
+import qualified Data.ByteString.Char8        as B
 import qualified Data.Text.Lazy               as L
 import           Data.Time.Calendar
 import           Data.Time.LocalTime
@@ -31,6 +32,9 @@ instance Arbitrary ZonedTime where
 
         zone = minutesToTimeZone <$> choose (-12*60, 14*60)
 
+option :: Gen a -> Gen (Maybe a)
+option gen = frequency [(1, pure Nothing), (3, Just <$> gen)]
+
 char :: Gen Char
 char = frequency
     [ (9, elements [' ' .. '~'])
@@ -40,8 +44,27 @@ char = frequency
 text :: Gen L.Text
 text = L.pack <$> listOf char
 
-text1 :: Gen L.Text
-text1 = L.pack <$> listOf1 char
+phrase :: Gen L.Text
+phrase = L.pack <$> listOf1 char
+
+addrSpec :: Gen B.ByteString
+addrSpec = B.pack . concat <$> sequence [dotAtom, pure "@", dotAtom]  -- TODO
+  where
+    dotAtom = resize 20 . listOf1 $ elements atext
+    atext   = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "!#$%&'*+-/=?^_`{|}~"
+
+instance Arbitrary Address where
+    arbitrary = Address <$> addrSpec
+
+instance Arbitrary Mailbox where
+    arbitrary = Mailbox <$> option phrase <*> arbitrary
+
+instance Arbitrary Recipient where
+    arbitrary = oneof
+        [Individual <$> arbitrary, Group <$> phrase <*> listOf1 arbitrary]
+
+instance Arbitrary MessageID where
+    arbitrary = MessageID <$> addrSpec
 
 instance Arbitrary R.RenderOptions where
     arbitrary = R.RenderOptions
@@ -55,6 +78,9 @@ instance Arbitrary Converter where
 
 instance Arbitrary R.Encoding where
     arbitrary = arbitraryBoundedEnum
+
+list1 :: Arbitrary a => Gen [a]
+list1 = listOf1 arbitrary
 
 roundTrip
     :: (Eq a, Show a)
@@ -73,11 +99,31 @@ roundTrip name gen renderer parser = testProperty name $
 parsers :: TestTree
 parsers = testGroup "round trip"
     [ -- Origination date
-      roundTrip "Date"     arbitrary      R.date     P.date
-      -- Informational fields
-    , roundTrip "Subject"  text           R.subject  P.subject
-    , roundTrip "Comments" text           R.comments P.comments
-    , roundTrip "Keywords" (listOf text1) R.keywords P.keywords
+      roundTrip "Date"     arbitrary R.date P.date
+      -- Originator
+    , roundTrip "From"     list1     R.from    P.from
+    , roundTrip "Sender"   arbitrary R.sender  P.sender
+    , roundTrip "Reply-To" list1     R.replyTo P.replyTo
+      -- Destination address
+    , roundTrip "To"       list1          R.to  P.to
+    , roundTrip "Cc"       list1          R.cc  P.cc
+    , roundTrip "Bcc"      (option list1) R.bcc P.bcc
+      -- Identification
+    , roundTrip "Message-ID"  arbitrary R.messageID  P.messageID
+    , roundTrip "In-Reply-To" list1     R.inReplyTo  P.inReplyTo
+    , roundTrip "References"  list1     R.references P.references
+      -- Informational
+    , roundTrip "Subject"  text             R.subject  P.subject
+    , roundTrip "Comments" text             R.comments P.comments
+    , roundTrip "Keywords" (listOf1 phrase) R.keywords P.keywords
+      -- Resent
+    , roundTrip "Resent-Date"       arbitrary      R.resentDate      P.resentDate
+    , roundTrip "Resent-From"       list1          R.resentFrom      P.resentFrom
+    , roundTrip "Resent-Sender"     arbitrary      R.resentSender    P.resentSender
+    , roundTrip "Resent-To"         list1          R.resentTo        P.resentTo
+    , roundTrip "Resent-Cc"         list1          R.resentCc        P.resentCc
+    , roundTrip "Resent-Bcc"        (option list1) R.resentBcc       P.resentBcc
+    , roundTrip "Resent-Message-ID" arbitrary      R.resentMessageID P.resentMessageID
     ]
 
 main :: IO ()
