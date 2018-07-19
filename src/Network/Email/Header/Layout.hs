8b8c9f8d0228f561ae48d34a4b35186ecabb13df
Saurabh Nanda <saurabh@vacationlabs.com>
2018-07-19 19:03:41 +0530

    fixing the stupid semigroup/monoid nonsense

--- src/Network/Email/Header/Layout.hs
+++ src/Network/Email/Header/Layout.hs
@@ -17,9 +17,12 @@ type LayoutStep a = Int -> (Int -> Bool, a)
 -- | An abstract type representing a lazy layout.
 newtype Layout a = Layout { runLayout :: LayoutStep a -> LayoutStep a }
 
+instance Semigroup (Layout a) where
+    (<>) a b = Layout $ runLayout a . runLayout b
+
 instance Monoid (Layout a) where
     mempty      = Layout id
-    mappend a b = Layout $ runLayout a . runLayout b
+    mappend     = (<>)
 
 -- | Run a layout with an initial position.
 layout :: Monoid a => Int -> Layout a -> a
